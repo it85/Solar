e2d727a3a793789d0b9e7e064ae16cc4e8d69d6d
Isaac Tyan <isaac.tyan@gmail.com>
2015-12-01 22:23:19 -0500

    Second commit

--- src/main/java/model/MovingIndicator.java
+++ src/main/java/model/MovingIndicator.java
@@ -1,5 +1,28 @@
 package model;
 
-public abstract class MovingIndicator {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public abstract class MovingIndicator {
+	
+	public Deque<Double> dataPoints;
+	public int period;
+	
+	public MovingIndicator(int period){
+		this.period = period;
+		this.dataPoints = new ArrayDeque<Double>();
+	}
+	
+	public abstract double getValue();
+	
+	protected void addDataPoint(double data){
+		this.dataPoints.addLast(data);		
+		removeHeadIfFull();
+	}
+	
+	private void removeHeadIfFull(){
+		if(this.dataPoints.size() > this.period){
+			this.dataPoints.removeFirst();
+		}
+	}
 }
--- src/main/java/model/RSI.java
+++ src/main/java/model/RSI.java
@@ -1,5 +1,31 @@
 package model;
 
-public class RSI {
+public class RSI extends MovingIndicator{
 
+	public RSI(int period) {
+		super(period);
+	}
+
+	@Override
+	public double getValue() {
+		return 100 - (100 / 1 + calculateRS());
+	}
+	
+	private double calculateRS() {
+		double gain = 0;
+		double loss = 0;
+		
+		for(double d : this.dataPoints){
+			if(d > 0){
+				gain += d;
+			}else{
+				loss += d;
+			}
+		}
+		
+		double avgGain = gain / this.period;
+		double avgLoss = loss / this.period;
+		
+		return avgGain/avgLoss;		
+	}
 }
